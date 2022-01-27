5f706836bd6b1a327c33aa44a40b31f50d87cc33
HaloTester <mrttrkgl9090@gmail.com>
2022-01-27 01:11:53 +0100

    reporting stuff added

--- src/test/java/com/opnlycrm/tests/LoginPageTest.java
+++ src/test/java/com/opnlycrm/tests/LoginPageTest.java
@@ -1,37 +1,61 @@
 package com.opnlycrm.tests;
 
 import com.opnlycrm.pages.LoginPage;
+import com.opnlycrm.utilities.ConfigurationReader;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
 
 public class LoginPageTest extends TestBase {
 
     @Test
     public void loginAsHelpdeskUser() {
+        extentLogger = report.createTest("Login With Help Desk User Credentials");
         LoginPage loginPage = new LoginPage();
+        extentLogger.info("Username: " + ConfigurationReader.get("helpdesk_username"));
+        extentLogger.info("Password: " + ConfigurationReader.get("helpdesk_password"));
+        extentLogger.info("Login as a help desk user");
         loginPage.loginAsHelpdesk();
 
         String expectedUrl = "https://qa.openlycrm.com/stream/?login=yes";
+        extentLogger.info("Verify that user is main page");
         Assert.assertEquals(driver.getCurrentUrl(),expectedUrl);
+
+        extentLogger.pass("PASSED");
     }
 
     @Test
     public void loginAsHumanResourceUser() {
+        extentLogger = report.createTest("Login With Human Resource User Credentials");
+
         LoginPage loginPage = new LoginPage();
+        extentLogger.info("Username: " + ConfigurationReader.get("humanresource_username"));
+        extentLogger.info("Password: " + ConfigurationReader.get("humanresource_password"));
+        extentLogger.info("Login as a human resource user");
         loginPage.loginAsHumanResource();
 
         String expectedUrl = "https://qa.openlycrm.com/stream/?login=yes";
+        extentLogger.info("Verify that user is main page");
         Assert.assertEquals(driver.getCurrentUrl(), expectedUrl);
+
+        extentLogger.pass("PASSED");
     }
 
     @Test
     public void loginAsMarketingUser() {
+        extentLogger = report.createTest("Login With Marketing User Credentials");
 
         LoginPage loginPage = new LoginPage();
+        extentLogger.info("Username: " + ConfigurationReader.get("marketing_username"));
+        extentLogger.info("Password: " + ConfigurationReader.get("marketing_password"));
+        extentLogger.info("Login as a marketing user");
         loginPage.loginAsMarketing();
 
         String expectedUrl = "https://qa.openlycrm.com/stream/?login=yes";
+        extentLogger.info("Verify that user is main page");
         Assert.assertEquals(driver.getCurrentUrl(), expectedUrl);
+
+        extentLogger.pass("PASSED");
     }
 
 }
--- src/test/java/com/opnlycrm/tests/TestBase.java
+++ src/test/java/com/opnlycrm/tests/TestBase.java
@@ -1,20 +1,31 @@
 package com.opnlycrm.tests;
 
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.opnlycrm.utilities.BrowserUtils;
 import com.opnlycrm.utilities.ConfigurationReader;
 import com.opnlycrm.utilities.Driver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class TestBase {
     protected WebDriver driver;
     protected Actions actions;
     protected WebDriverWait wait;
+    protected ExtentReports report;
+    protected ExtentHtmlReporter htmlReporter;
+    protected ExtentTest extentLogger;
 
     @BeforeMethod
     public void setUp() {
@@ -27,8 +38,35 @@ public class TestBase {
     }
 
     @AfterMethod
-    public void tearDown() throws InterruptedException {
+    public void tearDown(ITestResult result) throws InterruptedException, IOException {
+
+        if(result.getStatus()== ITestResult.FAILURE){
+            extentLogger.fail(result.getName());
+            String screenShotPath = BrowserUtils.getScreenshot(result.getName());
+            extentLogger.addScreenCaptureFromPath(screenShotPath);
+            extentLogger.fail(result.getThrowable());
+        }
         Thread.sleep(2000);
         Driver.closeDriver();
     }
+
+    @BeforeTest
+    public void setUpTest(){
+        report = new ExtentReports();
+        String projectPath = System.getProperty("user.dir");
+        String path = projectPath + "/test-output/report.html";
+        htmlReporter = new ExtentHtmlReporter(path);
+        report.attachReporter(htmlReporter);
+        htmlReporter.config().setReportName("Vytrack Smoke Test");
+        report.setSystemInfo("Environment","QA");
+        report.setSystemInfo("Browser", ConfigurationReader.get("browser"));
+        report.setSystemInfo("OS",System.getProperty("os.name"));
+    }
+
+    @AfterTest
+    public void tearDownTest(){
+        report.flush();
+    }
+
+
 }
